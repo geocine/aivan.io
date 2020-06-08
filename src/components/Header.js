c16b1d1e5d541614a60fbd9d7defba66db5a7c2c
Aivan Monceller <aivan@monceller.com>
2020-06-08 16:39:02 +0800

    feat(header): fix responsive styles of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import Logo from '../images/aivan-logo.svg'
 import styled from '@emotion/styled'
+import { bpDesktopOnly } from '../lib/breakpoints'
 
 const StyledHeader = styled.header`
   margin-bottom: 1.45rem;
-  height: 80px;
-  max-height: 80px;
+  height: 65px;
+  max-height: 65px;
+
+  ${bpDesktopOnly} {
+    height: 80px;
+    max-height: 80px;
+  }
 
   div {
     margin: 0 auto;
     max-width: 960;
-    padding: 0.5rem 1.0875rem;
     box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);
     background: #ffffff;
     text-align: center;
@@ -34,7 +39,10 @@ const StyledHeader = styled.header`
   }
 
   img {
-    width: 150px;
+    width: 120px;
+    ${bpDesktopOnly} {
+      width: 150px;
+    }
     margin-top: 5px;
   }
 `
