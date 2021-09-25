4b5ab95f6662ee9377c75618a47d67d414ba4c38
Aivan Monceller <aivan@monceller.com>
2021-09-26 00:08:02 +0800

    feat(image): update gatsby image to use statically.io

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import GatsbyImage from 'gatsby-image'
 
-const imageTransform = 'https://img.imageboss.me/aivan'
+const imageTransform = 'https://cdn.statically.io/img/cdn.aivan.io'
 
 const Image = ({ src, sizes, aspectRatio, ...rest }) => {
   return (
     <GatsbyImage
       fluid={{
         aspectRatio,
-        src: `${imageTransform}/cdn/${src}`,
-        base64: `${imageTransform}/width/30/blur:4/${src}`,
+        src: `${imageTransform}/${src}`,
+        base64: `${imageTransform}/w=30,q=4/${src}`,
         sizes: '(max-width: 850px) 100vw, 850px',
         srcSet: sizes
           .map(size => {
-            return `${imageTransform}/width/${size}/quality:100/${src} ${size}w`
+            return `${imageTransform}/w=${size},q=100/${src} ${size}w`
           })
           .join(),
       }}
