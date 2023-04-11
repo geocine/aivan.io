35cbf652b4c36abd5ae4aa22232a6bc2e963e0d2
Aivan Monceller <aivan@monceller.com>
2023-04-11 22:38:00 +0800

    fix(gtag): do not change origin from gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -93,10 +93,6 @@ const config: GatsbyConfig = {
           head: false,
           // Setting this parameter is also optional
           respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ['/preview/**', '/do-not-track/me/too/'],
-          // Defaults to https://www.googletagmanager.com
-          origin: 'aivan.io',
           // Delays processing pageview events on route update (in milliseconds)
           delayOnRouteUpdate: 0,
         },
--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 // import type { HeadFC } from "gatsby"
 
 // const imageHost = 'https://img.imageboss.me/aivan'
@@ -13,37 +13,33 @@ function SEO({
   title,
   image
 }) {
+  const data = useStaticQuery(detailsQuery)
+
+  const metaDescription =
+  description || data.site.siteMetadata.description
+const metaTitle = index
+  ? `${data.site.siteMetadata.title} | ${data.site.siteMetadata.shortDescription}`
+  : title
+
   return (
-    <StaticQuery
-      query={detailsQuery}
-      render={(data) => {
-        const metaDescription =
-          description || data.site.siteMetadata.description
-        const metaTitle = index
-          ? `${data.site.siteMetadata.title} | ${data.site.siteMetadata.shortDescription}`
-          : title
-        return (
-          <>
-            <html lang="en" />
-            <title>{metaTitle}</title>
-            <meta name="description" content={metaDescription} />
-            <meta property="og:title" content={metaTitle} />
-            <meta property="og:description" content={metaDescription} />
-            <meta property="og:type" content="website"/>
-            <meta property="og:image" content={`${imageHost}/${image}`} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta
-              name="twitter:creator"
-              content={data.site.siteMetadata.author}
-            />
-            <meta name="twitter:title" content={metaTitle} />
-            <meta name="twitter:image" content={`${imageHost}/${image}`} />
-            <meta name="twitter:description" content={metaDescription} />
-            <meta name="keywords" content={keywords.join(',')} />
-          </>
-        )
-      }}
-    />
+    <>
+      <html lang="en" />
+      <title>{metaTitle}</title>
+      <meta name="description" content={metaDescription} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:type" content="website"/>
+      <meta property="og:image" content={`${imageHost}/${image}`} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:creator"
+        content={data.site.siteMetadata.author}
+      />
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:image" content={`${imageHost}/${image}`} />
+      <meta name="twitter:description" content={metaDescription} />
+      <meta name="keywords" content={keywords.join(',')} />
+    </>
   )
 }
 
