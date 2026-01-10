fa463a64585f54c3043af3121c810e97699a13d5
Aivan Monceller <aivan@monceller.com>
2026-01-10 18:44:17 +0800

    refactor(image): simplify image source handling by using relative paths

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,21 +3,20 @@ import GatsbyImage from 'gatsby-image'
 
 // Cloudflare Image Transformations
 // Format: https://<CF-ZONE>/cdn-cgi/image/<OPTIONS>/<SOURCE-IMAGE>
-// Using cdn.aivan.io as the Cloudflare zone for transformations
+// Using relative paths since images are on the same domain
 const cfImageBase = 'https://cdn.aivan.io/cdn-cgi/image'
-const imageOrigin = 'https://cdn.aivan.io'
 
 const Image = ({ src, sizes, aspectRatio, ...rest }) => {
   return (
     <GatsbyImage
       fluid={{
         aspectRatio,
-        src: `${cfImageBase}/format=auto/${imageOrigin}/${src}`,
-        base64: `${cfImageBase}/width=30,quality=4,format=auto/${imageOrigin}/${src}`,
+        src: `${cfImageBase}/format=auto/${src}`,
+        base64: `${cfImageBase}/width=30,quality=4,format=auto/${src}`,
         sizes: '(max-width: 850px) 100vw, 850px',
         srcSet: sizes
           .map(size => {
-            return `${cfImageBase}/width=${size},quality=85,format=auto/${imageOrigin}/${src} ${size}w`
+            return `${cfImageBase}/width=${size},quality=85,format=auto/${src} ${size}w`
           })
           .join(),
       }}
